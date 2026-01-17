62ea412774ca5077bd5c8d72c8ecbef58bf063b7
Merertu Philipose <merertuphilip@gmail.com>
2026-01-17 12:17:57 +0300

    last commit

--- movie-review-backend/config/config/wsgi.py
+++ movie-review-backend/config/config/wsgi.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
-path = "/home/melody2203/Movie-Review-App/backend"
+# Add the project directory to the sys.path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if path not in sys.path:
     sys.path.append(path)
-
-os.environ["DJANGO_SETTINGS_MODULE"] = "Movie-Review-App.settings"
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
